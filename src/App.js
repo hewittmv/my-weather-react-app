0fd66d626cce29ffbc65f454cffd2e21e37f6972
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-09-18 14:45:13 +0100

    Create Search component with form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Weather App</h1>
+        <h1>Weather App </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <div className="cityWeather">
+            <Search />
+          </div>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Open Source Code by Victoria HM
         </a>
       </header>
     </div>
---
+++ src/Search.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+export default function Search() {
+  return (
+    <div className="form">
+      <form>
+        <input type="Search" placeholder="enter a city" /> {""}
+        <input type="Submit" value="Search" />
+      </form>
+    </div>
+  );
+}
