31e346f2dd0b9d8f1c002d232491760314b6a034
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-09-18 14:53:48 +0100

    Reduce header height & display H1 & React logo inline

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ function App() {
       <header className="App-header">
         <h1>Weather App </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <div className="container">
-          <div className="cityWeather">
-            <Search />
-          </div>
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Open Source Code by Victoria HM
-        </a>
       </header>
+      <div className="container">
+        <div className="cityWeather">
+          <Search />
+        </div>
+      </div>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Open Source Code by Victoria HM
+      </a>
     </div>
   );
 }
