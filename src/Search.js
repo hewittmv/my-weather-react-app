b31f8111761f24e2136e34eaf78d42a0bd4c302b
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-09-18 21:11:34 +0100

    increase height & width of search button

--- src/Search.js
+++ src/Search.js
@@ -10,7 +10,7 @@ export default function Search() {
           className="text-input"
         />{" "}
         {""}
-        <input type="Submit" value="Search" />
+        <input type="Submit" value="Search" className="search-button" />
       </form>
     </div>
   );
