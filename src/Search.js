6de3043a7f1c9cb549fd6e982f1a89b91289f421
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-09-18 20:58:05 +0100

    increase width of text input

--- src/Search.js
+++ src/Search.js
@@ -4,7 +4,12 @@ export default function Search() {
   return (
     <div className="form">
       <form>
-        <input type="Search" placeholder="enter a city" /> {""}
+        <input
+          type="Search"
+          placeholder="enter a city"
+          className="text-input"
+        />{" "}
+        {""}
         <input type="Submit" value="Search" />
       </form>
     </div>
