b36b28bbe2bc1f5c49c8de14096e6cf7b2bd818a
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-09-18 20:31:06 +0100

    Move github link into app footer

--- src/App.js
+++ src/App.js
@@ -5,37 +5,34 @@ import Search from "./Search";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <div className="container">
-        <div className="city-weather">
-          <Search />
-          <button></button>
-          <h6>Last Updated: 18/09/23 18:08 </h6>
-          <h2>New York</h2>
-          <p>🌤️ Sunny </p>
-          <div>
-            <ul>
-              <li>Min: 18°C</li>
-              <li>Max: 25°C </li>
-              <li>Wind: 5km/h</li>
-            </ul>
-          </div>
+      <header className="App-header">VHM</header>
+
+      <div className="city-weather">
+        <Search />
+        <button></button>
+        <h6>Last Updated: 18/09/23 18:08 </h6>
+        <h2>New York</h2>
+        <p>🌤️ Sunny </p>
+        <div>
+          <ul>
+            <li>Min: 18°C</li>
+            <li>Max: 25°C </li>
+            <li>Wind: 5km/h</li>
+          </ul>
         </div>
       </div>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Open Source Code by Victoria HM
-      </a>
+
       <footer className="App-footer">
         <h3>Weather App</h3>
         <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="github-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open Source Code by Victoria HM
+        </a>
       </footer>
     </div>
   );
